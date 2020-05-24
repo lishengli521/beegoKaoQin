972300f0e2c1155ea9f24c637ed6206eb7ca1b9f
ligang <123456>
2020-05-24 18:53:00 +0800

    提交

--- controllers/UserController.go
+++ controllers/UserController.go
@@ -1,9 +1,11 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/astaxie/beego"
 	"mysqlgo/models"
+	"mysqlgo/utils"
 )
 type JSONS1 struct {
 	//必须的大写开头
@@ -16,11 +18,16 @@ type UserController struct {
 }
 func (c *UserController) Get() {
 	var user =models.GetAll()
-	fmt.Println("--------------------")
-	fmt.Println(user)
-	fmt.Println("--------------------")
-	 data := &JSONS1{"200", "获取成功",  user}
+	test := utils.RDSTest
+	values,_:=json.Marshal(user)
+	test.Set("users",string(values))
+	var users,_=test.Get("users")
+	var aType []models.User
+	json.Unmarshal(users.([]byte),&aType)
+	fmt.Println("---------redis-存值----------")
+	fmt.Println(aType)
+	fmt.Println("---------redis取值-----------")
+	data := &JSONS1{"200", "获取成功",  aType}
 	c.Data["json"] = data
 	c.ServeJSON()
-	//c.Ctx.WriteString("mysqldo=====test")
 }
